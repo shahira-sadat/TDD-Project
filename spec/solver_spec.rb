f91180df7d86cd79a1712631fb7dd0f7aabbabee
Nyame-Wolf <mumenyam@gmail.com>
2022-10-06 19:56:06 +0300

    used self to not need instance of classes

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,7 +1,6 @@
 require_relative '../solver'
 
 describe Solver do
-  solve = Solver.new
   number1 = 5
   number2 = 0
   number3 = -1
@@ -15,43 +14,43 @@ describe Solver do
 
   context 'Test the factorial method in Solver class' do
     it 'should return the factorial of 5' do
-      expect(solve.factorial(number1)).to eq(120)
+      expect(Solver.factorial(number1)).to eq(120)
     end
 
     it 'should return the factorial of 1' do
-      expect(solve.factorial(number2)).to eq(1)
+      expect(Solver.factorial(number2)).to eq(1)
     end
 
     it 'should return the factorial of -1' do
-      expect { solve.factorial(number3) }.to raise_error(NegativeNumberError, 'Number is less than 0')
+      expect { Solver.factorial(number3) }.to raise_error(NegativeNumberError, 'Number is less than 0')
     end
   end
 
   context 'Test the reverse string method in Solver class' do
     it 'should return the reversed hello' do
-      expect(solve.reversed(word1)).to eq('olleh')
+      expect(Solver.reversed(word1)).to eq('olleh')
     end
 
     it 'should return the reversed bye' do
-      expect(solve.reversed(word2)).to eq('eyb')
+      expect(Solver.reversed(word2)).to eq('eyb')
     end
   end
 
   context 'Test the fizzbuzz method in Solver class' do
     it 'should return fizz' do
-      expect(solve.fizzbuzz(number4)).to eq('fizz')
+      expect(Solver.fizzbuzz(number4)).to eq('fizz')
     end
 
     it 'should return buzz' do
-      expect(solve.fizzbuzz(number5)).to eq('buzz')
+      expect(Solver.fizzbuzz(number5)).to eq('buzz')
     end
 
     it 'should return fizzbuzz' do
-      expect(solve.fizzbuzz(number6)).to eq('fizzbuzz')
+      expect(Solver.fizzbuzz(number6)).to eq('fizzbuzz')
     end
 
     it 'should return buzz' do
-      expect(solve.fizzbuzz(number7)).to eq('8')
+      expect(Solver.fizzbuzz(number7)).to eq('8')
     end
   end
 end
