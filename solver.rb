513ee193c4715b7cdc896123fa2bc0804e0bc2c4
Nyame-Wolf <mumenyam@gmail.com>
2022-10-06 20:01:42 +0300

    add a method to reverse strings

--- solver.rb
+++ solver.rb
@@ -3,6 +3,10 @@ class Solver
     def self.factorial(n)
         n < 0 ? (raise NegativeNumberError, 'Number is less than 0') : (1..n).reduce(1, :*)
     end
+
+    def self.reversed(word)
+        word.reverse
+    end
 end
 
 
