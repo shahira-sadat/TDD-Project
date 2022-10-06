5e2c20682d6579b90ad8750b2950c1c85292e151
Nyame-Wolf <mumenyam@gmail.com>
2022-10-06 19:57:44 +0300

    add factorial method
    add a NegativeNumberError to be raised incase factorial is less than 0

--- solver.rb
+++ solver.rb
@@ -1,2 +1,8 @@
+require_relative './negative_error'
 class Solver
+    def self.factorial(n)
+        n < 0 ? (raise NegativeNumberError, 'Number is less than 0') : (1..n).reduce(1, :*)
+    end
 end
+
+
