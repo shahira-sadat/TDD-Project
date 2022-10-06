a14854f1684a2ce488f8a2d321696c540fe5f108
shahira-sadat <shahira.sadat1@gmail.com>
2022-10-06 21:41:18 +0430

    Add fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -1,12 +1,22 @@
 require_relative './negative_error'
 class Solver
-    def self.factorial(n)
-        n < 0 ? (raise NegativeNumberError, 'Number is less than 0') : (1..n).reduce(1, :*)
-    end
+  def self.factorial(num)
+    num.negative? ? (raise NegativeNumberError, 'Number is less than 0') : (1..num).reduce(1, :*)
+  end
 
-    def self.reversed(word)
-        word.reverse
+  def self.reversed(word)
+    word.reverse
+  end
+
+  def self.fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
     end
+  end
 end
-
-
