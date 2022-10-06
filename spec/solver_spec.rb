6c142c81f044f89f0e807a30d062172437f69d13
shahira-sadat <shahira.sadat1@gmail.com>
2022-10-06 20:55:50 +0430

    Add test for reversed string method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,20 +5,30 @@ describe Solver do
   number1 = 5
   number2 = 0
   number3 = -1
+  word1 = 'hello'
+  word2 = 'bye'
 
-context 'Test the factorial method in Solver class' do
-  it 'should return the factorial of given number' do
-    expect(solve.factorial(number1)).to eq (120)
+  context 'Test the factorial method in Solver class' do
+    it 'should return the factorial of 5' do
+      expect(solve.factorial(number1)).to eq(120)
+    end
+
+    it 'should return the factorial of 1' do
+      expect(solve.factorial(number2)).to eq(1)
+    end
+
+    it 'should return the factorial of -1' do
+      expect { solve.factorial(number3) }.to raise_error(NegativeNumberError, 'Number is less than 0')
+    end
   end
 
-  it 'should return the factorial of given number' do
-    expect(solve.factorial(number2)).to eq (1)
-  end
+  context 'Test the reverse string method in Solver class' do
+    it 'should return the reversed hello' do
+      expect(solve.reversed(word1)).to eq('olleh')
+    end
 
-  it 'should return the factorial of given number' do
-    expect{solve.factorial(number3)}.to raise_error(NegativeNumberError, 'Number is less than 0')
+    it 'should return the reversed bye' do
+      expect(solve.reversed(word2)).to eq('eyb')
+    end
   end
 end
-
-
-
